e1be604111fb857359e708cc3cee99049c1cb2f0
Eric Scheier <eric@scheier.org>
2017-02-13 11:35:43 +0200

    add more setup instructions

--- setup.R
+++ setup.R
@@ -4,11 +4,13 @@ token <- gs_auth(cache = FALSE)
 gd_token()
 saveRDS(token, file = "googlesheets_token.rds")
 
-# If you haven't connected your Rstudio to Shinyapps.io, use the following commented commands
-# require('devtools')
-# devtools::install_github('rstudio/shinyapps')
-# require('shinyapps')
-# shinyapps::setAccountInfo(name='yourUserNameHere', token='yourTokenHere', secret='yourSecretHere')
+# If you haven't connected your Rstudio to Shinyapps.io, use the following commands
+# You should only have to do this once
+require('devtools')
+devtools::install_github('rstudio/shinyapps')
+require('shinyapps')
+shinyapps::setAccountInfo(name='yourUserNameHere', token='yourTokenHere', secret='yourSecretHere')
 
+# Deploy the application
 library(rsconnect)
 deployApp()
